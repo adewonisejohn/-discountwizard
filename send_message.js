077b4ff8b619047046d29f241f1b48eb4f3fdac2
adewonisejohn <adewonisekayode@gmail.com>
2023-09-14 14:09:52 +0100

    this commit removes my token

--- send_message.js
+++ send_message.js
@@ -1,5 +1,5 @@
-const accountSid = 'AC73fd9c36225c0c4cc904643688d2b0db';
-const authToken = '6c4910ba57b6240d717ca86b33212005';
+const accountSid = 'your twilio account sid';
+const authToken = 'your tiwlio auth token';
 
 const client = require('twilio')(accountSid, authToken);
 
@@ -7,7 +7,7 @@ function send_message(message,send_to){
 
     client.messages
         .create({
-            from: 'whatsapp:+14155238886',
+            from: 'twilio whatsap number',
             body:message,
             to: send_to
         })
